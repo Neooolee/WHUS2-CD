cbdc50850a7b4100d6d0cb600714dad56ef7d2c3
jun, li <42463488+Neooolee@users.noreply.github.com>
2022-09-01 09:47:18 +0800

    Update cutimg.py

--- cutimg.py
+++ cutimg.py
@@ -11,24 +11,23 @@ import glob
 from gdaldiy import *
 
 """
-The source file should be organized as follows:
-./train/10m/file1.tif   (4 bands 2/3/4/8)
-./train/20m/file1.tif   (6 bands 5/6/7/8A/11/12)
-./train/60m/file1.tif   (3 bands 1/9/10)
+The source file should be organized as follows by running fuseimg.py first:
+sourcedir
+        /10m/file1.tif...filen.tif   (4 bands 2/3/4/8)
+        /20m/file1.tif...filen.tif   (6 bands 5/6/7/8A/11/12)
+        /60m/file1.tif...filen.tif   (3 bands 1/9/10)
 """
  
-sourcedir='./train/'#image dir such as    
-name='10m'#20m, 60m
-sourcepath=sourcedir+name
+sourcedir='F:/WHU/WHUS2-CD+/composite/'#source dir  
+names=['10m','20m','60m']
+window_sizes,strides=[384,192,64],[384,192,64]
 
-def cut_data(filedir):
-    window_size=384 #384 for 10 m bands, 192 for 20 m bands, 64 for 60 m bands.
-    stride=384#384 for 10 m bands, 192 for 20 m bands, 64 for 60 m bands.
+def cut_data(filedir,window_size,stride):
     filedirs=glob.glob(os.path.join(filedir, '*'))
     for i in range(len(filedirs)):
         filepath=filedirs[i]
         print(filepath)
-        savedirname=filepath.split('\\')[-1].split('.tif')[0][27:44]
+        savedirname=filepath.split('\\')[-1].split('.tif')[0][33:44]
         savedirpath=filedir+'clips\\'+savedirname
         if not os.path.exists(savedirpath):
             os.makedirs(savedirpath)
@@ -44,13 +43,11 @@ def cut_data(filedir):
                 n=n+1
                 if np.all(img[high:high+window_size,width:width+window_size]>0):
                     imgwrite(savedirpath+"/"+str(n)+'.tif',img[high:high+window_size,width:width+window_size])
-# cut_data(sourcepath)
-def multi_dir(path):
-    filedirs=glob.glob(os.path.join(path, '*'))
-    for i in range(len(filedirs)):
-        filedir=filedirs[i]       
-        cut_data(filedir)
-multi_dir(sourcepath)
+def multi_dir(filedir,window_size,stride):
+    print(filedir)      
+    cut_data(filedir,window_size,stride)
+for i in range(len(names)):  
+    multi_dir(sourcedir+names[i],window_sizes[i],strides[i])
 
 
 
